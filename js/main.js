5b6bad60948fd035de500ee7f51e734dee62c8cc
Anushree Brahmacharimayum <37607778+abembmyum@users.noreply.github.com>
2024-08-06 11:45:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = event.target.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
 
